fc5d035ceaefe6ca15ea8c2845f16ba579acb7ef
JessyMakkry <jessy.makkry@bk.ru>
2024-03-23 16:54:39 +0300

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Задание 1
+        int empire = 10000000;
+        byte a = 12;
+        short crouch = 1337;
+        long lion = 57L;
+        float nice = 3.22f;
+        double trouble = 14.9320d;
+        System.out.println("Значение переменной empire с типом int равно "+empire);
+        System.out.println("Значение переменной a с типом byte равно "+a);
+        System.out.println("Значение переменной crouch с типом short равно "+crouch);
+        System.out.println("Значение переменной lion с типом long равно "+lion);
+        System.out.println("Значение переменной nice с типом float равно "+nice);
+        System.out.println("Значение переменной trouble с типом double равно "+trouble);
     }
 }
